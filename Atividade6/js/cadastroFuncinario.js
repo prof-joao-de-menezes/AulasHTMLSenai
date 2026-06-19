3641276d38dbe3eaf7459ce62bc4fb38bc771b2c
Joao <joao.m.junior7@docente.senai.br>
2026-06-18 21:38:50 -0300

    fix erro sintaxe

--- Atividade6/js/cadastroFuncinario.js
+++ Atividade6/js/cadastroFuncinario.js
@@ -1,6 +1,6 @@
 function cadastrarFuncionario() {
     let nome = document.getElementById("nome-funcionario").value;
-    let horas = document.getElementById("horario").value;
+    let horas = document.getElementById("horas-funcionario").value;
     let cargo = document.getElementById("cargo-funcionario").value;
 
     console.log(nome)
