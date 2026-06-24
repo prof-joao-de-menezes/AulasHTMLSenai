0826b33818cd065025d3c34a07974bd203d31710
Joao <joao.m.junior7@docente.senai.br>
2026-06-23 21:26:47 -0300

    Fix: sintaxe

--- Atividade8/js/funcoes.js
+++ Atividade8/js/funcoes.js
@@ -9,28 +9,6 @@ function soma() {
     salvarHistorico("Soma",primeiroNumero, segundoNumero, resultado)
 }
 
-function subtracao() {
-    let primeiroNumero = document.getElementById("primeiro-numero").value;
-    let segundoNumero = document.getElementById("segundo-numero").value;
-
-    let resultado = Number(primeiroNumero) - Number(segundoNumero);
-
-    document.getElementById("resultado").innerText = resultado;
-
-    salvarHistorico("Soma",primeiroNumero, segundoNumero, resultado)
-}
-
-function divisao() {
-    let primeiroNumero = document.getElementById("primeiro-numero").value;
-    let segundoNumero = document.getElementById("segundo-numero").value;
-
-    let resultado = Number(primeiroNumero) / Number(segundoNumero);
-
-    document.getElementById("resultado").innerText = resultado;
-
-    salvarHistorico("Soma",primeiroNumero, segundoNumero, resultado)
-}
-
 function salvarHistorico(nomeDaFuncao, num1, num2, resultado) {
     console.log("Função: " + nomeDaFuncao);
     console.log("Primeiro numero: " + num1 + ", SegundoNumero: " + num2);
